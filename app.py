9c7ef6561c6107efd605aec063bbda0bd3a4b2f9
Átila Prudente <150717036+Chossei@users.noreply.github.com>
2025-08-01 09:39:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title('Aplicativo com login')
+paginas = {
+  'Páginas': [st.Page('paginas/pagina1.py',default = True)],
+  'Exemplo': [st.Page('paginas/pagina2.py',default = False)]
+}
+
+
+# para navegar entre as páginas
+pg = st.navigation(paginas)
+pg.run()
